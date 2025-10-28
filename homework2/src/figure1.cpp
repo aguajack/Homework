77bbb05cd9a908ae44ddac0c03f7ad3bb3358c86
aguajack <41343147@nfu.edu.tw>
2025-10-28 17:51:20 +0800

    Update figure1.cpp

--- homework2/src/figure1.cpp
+++ homework2/src/figure1.cpp
@@ -16,7 +16,8 @@ private:
 	Polynomial();                      // 建構 p(x)=0
 	Polynomial Add(Polynomial poly);   // 回傳 this + poly
 	Polynomial Mult(Polynomial poly);  // 回傳 this × poly
-	float Eval(float f);               // 代入 f 計算 p(f)
+	float Eval(float f);   // 代入 f 計算 p(f)
+	void newTerm(const float newcoef, const int newwxp);
 	friend istream& operator>>(istream& input, Polynomial& poly); // 輸入
 	friend ostream& operator<<(ostream& output, const Polynomial& poly); // 輸出
 };
@@ -24,12 +25,40 @@ Polynomial::Polynomial() {
 	capacity = 2;
 	terms = 0;
 	termArray = new Term[capacity];
-	}
-Polynomial::Add() {
+}
+Polynomial Polynomial::Add(Polynomial poly) {
+	Polynomial result;
+	int aPos = 0;
+	int bPos = 0;
+	while (aPos < terms && poly.terms) {
+		if (termArray[aPos].exp == poly.termArray[bPos].exp) {
+			float sumcoef = termArray[aPos].coef + termArray[bPos].coef;
+			if (sumcoef != 0)
+				result.newTerm(sumcoef, termArray[aPos].exp);
+			aPos++;
+			bPos++;
+		}
+		else if (termArray[aPos].exp > termArray[bPos].exp) {
+			result.newTerm(termArray[aPos].coef, termArray[aPos].exp);
+				aPos++;
+		}
+		else {
+			result.newTerm(poly.termArray[bPos].coef, poly.termArray[bPos].exp);
+			bPos++;
+		}
 
+
+	}
+}
+void Polynomial::newTerm(const float newCoef, const int newExp) {
 
 
 }
+Polynomial Polynomial::Mult(Polynomial) {
+
+
+}
+
 
 
 
